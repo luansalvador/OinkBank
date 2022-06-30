46c298ed2b47d87884dd22ec327569c56804251a
VillarDeveloper <villar.developer@gmail.com>
2022-06-30 11:18:06 -0700

    update mask email

---
+++ CRUD MVVM/CRUD MVVM/Utils/Extensions/Extension+StringLowercased.swift
@@ -0,0 +1,21 @@
+//
+//  Extension+StringLowercased.swift
+//  CRUD MVVM
+//
+//  Created by user220831 on 6/30/22.
+//
+
+import Foundation
+import UIKit
+
+extension RegisterViewController {
+
+func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        if textField.tag == 10 {
+            textField.text = (textField.text! as NSString).replacingCharacters(in: range, with: string.lowercased())
+            return false
+        }
+        return true
+    }
+}
