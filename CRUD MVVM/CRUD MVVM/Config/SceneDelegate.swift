f7ef3ca87e0235ecadf5d9c291b7716dfb94cc9b
andreza-evangelista <andreza.evangel@gmail.com>
2022-07-04 08:19:19 -0700

    Commit: Tela inicial alterada para de login.

--- CRUD MVVM/CRUD MVVM/Config/SceneDelegate.swift
+++ CRUD MVVM/CRUD MVVM/Config/SceneDelegate.swift
@@ -23,7 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
 
  
-        let rootViewController = RegisterSucessViewController()
+        let rootViewController = LoginViewController()
 
 
         
